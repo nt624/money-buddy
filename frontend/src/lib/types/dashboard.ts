c7c1d6cc9d2ba43391c19ba537be9fab738e4ffc
nt624 <taku8606.uec@gmail.com>
2026-02-05 08:24:57 +0900

    #52 dashboard型を追加する

---
+++ frontend/src/lib/types/dashboard.ts
@@ -0,0 +1,9 @@
+export type Dashboard = {
+  income: number
+  saving_goal: number
+  fixed_costs: number
+  variable_budget: number
+  confirmed_expenses: number
+  planned_expenses: number
+  remaining: number
+}
